713ce07fb6b3acd6a7f920609c6d8150d598697d
minutaurus <made_power@naver.com>
2024-01-13 18:22:27 +0900

    refactor : 서비스 로직 수정
    기존에 재사용 코드가 너무 많아 메서드로 추출하였습니다. 목록은 다음과 같습니다.
    - Repository에서 멤버를 조회하는 로직.
    - FileUtil을 사용하기 위한 로직

--- sosik-memberservice/src/main/java/com/example/sosikmemberservice/service/OAuthService.java
+++ sosik-memberservice/src/main/java/com/example/sosikmemberservice/service/OAuthService.java
@@ -1,8 +1,11 @@
 package com.example.sosikmemberservice.service;
 
+import com.example.sosikmemberservice.dto.request.RequestUpdateOAuthMember;
 import com.example.sosikmemberservice.dto.response.oauth.kakao.ResponseKakao;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface OAuthService {
     ResponseKakao createMemberByOauth(String code);
+    void updateOAuthMember(Long memberId, RequestUpdateOAuthMember updateMember, MultipartFile profileImage);
 
 }
--- sosik-memberservice/src/main/java/com/example/sosikmemberservice/service/OAuthServiceImpl.java
+++ sosik-memberservice/src/main/java/com/example/sosikmemberservice/service/OAuthServiceImpl.java
@@ -2,28 +2,38 @@ package com.example.sosikmemberservice.service;
 
 import com.example.sosikmemberservice.config.oauth.kakao.KakaoTokenJsonData;
 import com.example.sosikmemberservice.config.oauth.kakao.KakaoUserInfo;
+import com.example.sosikmemberservice.dto.request.RequestUpdateOAuthMember;
 import com.example.sosikmemberservice.dto.response.oauth.kakao.ResponseKakao;
 import com.example.sosikmemberservice.dto.response.oauth.kakao.ResponseKakaoToken;
 import com.example.sosikmemberservice.dto.response.oauth.kakao.ResponseKakaoUserInfo;
+import com.example.sosikmemberservice.exception.ApplicationException;
+import com.example.sosikmemberservice.exception.ErrorCode;
 import com.example.sosikmemberservice.model.entity.MemberEntity;
+import com.example.sosikmemberservice.model.entity.WeightEntity;
 import com.example.sosikmemberservice.model.vo.Email;
 import com.example.sosikmemberservice.repository.MemberRepository;
 import com.example.sosikmemberservice.repository.RefreshTokenRepository;
+import com.example.sosikmemberservice.util.file.FileUtils;
+import com.example.sosikmemberservice.util.file.ResultFileStore;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
-public class OAuthServiceImpl implements OAuthService{
+public class OAuthServiceImpl implements OAuthService {
     private final BCryptPasswordEncoder encoder;
     private final MemberRepository memberRepository;
     private final RefreshTokenRepository refreshTokenRepository;
     private final KakaoTokenJsonData kakaoTokenJsonData;
+    private final FileUtils filestore;
     private final KakaoUserInfo kakaoUserInfo;
-    public ResponseKakao createMemberByOauth(String code){
+
+    public ResponseKakao createMemberByOauth(String code) {
 
         ResponseKakaoToken kakaoTokenResponse = kakaoTokenJsonData.getToken(code);
         ResponseKakaoUserInfo userInfo = kakaoUserInfo.getUserInfo(kakaoTokenResponse.accessToken());
@@ -34,6 +44,8 @@ public class OAuthServiceImpl implements OAuthService{
             return ResponseKakao.builder()
                     .token(kakaoTokenResponse)
                     .info(userInfo)
+                    .memberId(existingMember.get().getMemberId())
+                    .isFirst(false)
                     .build();
         }
         MemberEntity member = MemberEntity.builder()
@@ -42,20 +54,46 @@ public class OAuthServiceImpl implements OAuthService{
                 .email(userInfo.kakaoAccount().email())
                 .nickname(userInfo.kakaoAccount().profile().nickname())
                 .birthday("2000-01-01")
-                .profileImage(userInfo.kakaoAccount().profile().profileImageUrl())
+                .profileImage("Create-Image")
                 .build();
+
         saveRefreshToken(kakaoTokenResponse, userInfo);
         memberRepository.save(member);
+
         return ResponseKakao.builder()
                 .token(kakaoTokenResponse)
                 .info(userInfo)
+                .isFirst(true)
+                .isEnrolled(false)
+                .memberId(member.getMemberId())
                 .build();
-     }
+    }
 
+    @Override
+    public void updateOAuthMember(Long memberId, RequestUpdateOAuthMember updateMember, MultipartFile profileImage) {
+        MemberEntity member = getMemberEntity(memberId);
+        ResultFileStore resultFileStore = null;
+        try {
+            resultFileStore = filestore.storeProfileFile(profileImage);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        member.updateProfileUrl(resultFileStore);
+        WeightEntity weightEntity = WeightEntity.create(updateMember.currentWeight(), updateMember.targetWeight());
+        weightEntity.addMember(member);
+
+        member.updateOAuthMember(updateMember);
+    }
 
     private void saveRefreshToken(ResponseKakaoToken kakaoTokenResponse, ResponseKakaoUserInfo userInfo) {
         refreshTokenRepository.save(kakaoTokenResponse.refreshToken(),
-                userInfo.kakaoAccount().email()+"OAuth");
+                userInfo.kakaoAccount().email() + "OAuth");
+    }
+
+    private MemberEntity getMemberEntity(Long memberId) {
+        return memberRepository.findById(memberId).orElseThrow(
+                () -> new ApplicationException(ErrorCode.USER_NOT_FOUND)
+        );
     }
 
 
